76dde23ae4a101ae5f869d1e6d9852d11f2f9a05
Tyler Wade <vaxonreal@gmail.com>
2025-01-13 18:43:23 -0500

    added logout route

--- backend/routes/auth.routes.ts
+++ backend/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getMe, login, signup } from '../controllers/auth.controller';
+import { getMe, login, logout, signup } from '../controllers/auth.controller';
 import protectedRoute from '../middleware/protectedRoute';
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.delete("/logout", logout);
 router.get("/", protectedRoute, getMe);
 
 export default router;
